0382b3aac5ee9bc785f750d3295509a96928b1fc
Talha Abdulkuddus <30045102+Talha-Ak@users.noreply.github.com>
2024-06-20 14:11:27 +0300

    Add completion options

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Enable Nerd Fonts
+vim.g.have_nerd_font = true
+
 -- Line Numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -12,8 +15,12 @@ vim.opt.mouse = "a"
 -- Don't show mode in command line
 vim.opt.showmode = false
 
--- Sync OS & Vim clipboard
-vim.opt.clipboard = "unnamedplus"
+-- -- Sync OS & Vim clipboard
+-- -- If disabled, use "+y and "+p for system clipboard
+-- vim.opt.clipboard = "unnamedplus"
+
+-- Wrapped lines stay visually indented
+vim.opt.breakindent = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.opt.ignorecase = true
@@ -40,6 +47,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Highlight search
+vim.opt.hlsearch = true
+
 -- Indentation
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
@@ -57,6 +67,9 @@ vim.opt.virtualedit = "block"
 
 -- Enable 24-bit colour
 vim.opt.termguicolors = true
+
+-- Completion options
+vim.opt.completeopt = "menu,menuone,noselect"
 
 -- Completion height
 vim.opt.pumheight = 10
